e1999f3caf97dc4e896b74f53474c60271d257dd
pablo adrian <etereosum@gmail.com>
2019-03-10 01:27:45 -0500

    first commit

---
+++ JDBC/src/introduccionjdbc/IntroduccionJDBC.java
@@ -0,0 +1,43 @@
+
+package introduccionjdbc;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class IntroduccionJDBC {
+
+
+    public static void main(String[] args) {
+        //Cadena de conexión de mysql, el parametro use SSL es opcional
+        String url = "jdbc:mysql://localhost:3306/sga?useSSL=false";
+          
+        try{
+            //Cargar el driver de mysql
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            //Creamos el objeto de conexion
+            Connection conexion = (Connection) DriverManager.getConnection(url,"root", "root");
+            
+            //Creamos un objeto de tipo Statement (sentencias)
+            Statement instruccion = conexion.createStatement();
+            
+            //Creamos el query
+            String sql = "select id_persona, nombre, apellido from persona";
+           
+            ResultSet result = instruccion.executeQuery(sql);
+            
+            while(result.next()) {
+                System.out.print("Id: "+result.getInt(1));
+                System.out.print(" Nombre: "+result.getString(2));
+                System.out.println(" Apellido: "+result.getString(2));
+            }
+            //Cerrar cada uno de los objetos utilizados
+            result.close();
+            instruccion.close();
+            conexion.close();
+            
+        }  catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
